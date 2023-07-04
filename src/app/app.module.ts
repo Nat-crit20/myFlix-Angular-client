e14e1fd7c58413bd0125823bf97ae333b6448535
Nat-crit20 <greuelnathan74@gmail.com>
2023-07-04 08:47:15 -0500

    Created user registration form and methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+
+// const appRoutes: Routes = [
+//   { path: 'welcome', component: WelcomePageComponent },
+//   { path: 'movies', component: MovieCardComponent },
+//   { path: 'user', component: UserProfileComponent },
+//   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+// ];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, UserRegistrationFormComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    FormsModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
