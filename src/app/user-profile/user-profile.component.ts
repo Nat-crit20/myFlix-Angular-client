4d3fccd2a306870c15a47d5f9f6361a9b3ab129e
Nat-crit20 <greuelnathan74@gmail.com>
2023-07-05 08:33:55 -0500

    Created getUser for the profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+import { UserRegistrationService } from '../fetch-api-data.service';
+import { formatDate } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  user: any = {};
+  favoriteMovies: any[] = [];
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
 
+  constructor(
+    public fetchApi: UserRegistrationService,
+    public route: Router,
+    public snackbar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {}
+
+  getUser(): void {
+    this.user = this.fetchApi.getUser();
+    this.userData.Username = this.user.Username;
+    this.userData.Email = this.user.Email;
+    this.userData.Birthday = formatDate(
+      this.user.Birthday,
+      'yyyy-MM-dd',
+      'en-US',
+      'UTC+0'
+    );
+    this.fetchApi.getAllMovies().subscribe((resp: any) => {
+      this.favoriteMovies = resp.filter((movie: { _id: any }) => {
+        return this.user.FavoriteMovies.indexOf(movie._id) >= 0;
+      });
+    });
+  }
 }
